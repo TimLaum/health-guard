d9220291ddc1145abb29bfdfba2c777b5a2106da
TLaum <timothee.laumonier@gmail.com>
2026-02-10 11:47:34 +0100

    Added jwt token routes

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from .db import init_db
 from dotenv import load_dotenv
+from flask_jwt_extended import JWTManager 
 import os
 
 def create_app():
@@ -10,8 +11,11 @@ def create_app():
     
     app.config["MONGO_URI"] = os.getenv("MONGO_URI", "mongodb://localhost:27017/healthguard")
     app.config["UPLOAD_FOLDER"] = "./uploads"
+    app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY", "super-secret-key-a-changer")
 
     init_db(app)
+
+    jwt = JWTManager(app)
 
     from .routes import main_bp
     app.register_blueprint(main_bp)
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,6 +1,8 @@
+from datetime import timedelta
 from flask import Blueprint, request, jsonify
 from .services import analyze_image
 import os
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
 main_bp = Blueprint('main', __name__)
 
@@ -8,8 +10,19 @@ main_bp = Blueprint('main', __name__)
 def health_check():
     return jsonify({"status": "active", "service": "HealthGuard Vision API"}), 200
 
+@main_bp.route('/re-auth' , methods=['POST'])
+@jwt_required()
+def re_auth():
+    current_user = get_jwt_identity()
+    print(f"Utilisateur connecté : {current_user}")
+    access_token = create_access_token(identity=current_user, expires_delta=timedelta(hours=24))
+    return jsonify({"token": access_token}), 200
+
 @main_bp.route('/predict', methods=['POST'])
+@jwt_required()
 def predict():
+    current_user = get_jwt_identity()
+    print(f"Utilisateur connecté : {current_user}")
     if 'image' not in request.files:
         return jsonify({"error": "Aucune image envoyée"}), 400
     
@@ -21,3 +34,20 @@ def predict():
         return jsonify(result), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+    
+
+@main_bp.route('/auth', methods=['POST'])
+def authenticate():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+
+
+    if username == 'admin' and password == 'password':
+        access_token = create_access_token(identity=username, expires_delta=timedelta(hours=24))
+        return jsonify({"token": access_token}), 200
+    else:
+        return jsonify({"error": "Invalid credentials"}), 401
+    
+

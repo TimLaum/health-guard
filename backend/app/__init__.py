82e1fe77be318c2774f49bd35e6712b3d9462ace
mohamedsellak <mohamed._.sellak@hotmail.com>
2026-02-13 10:49:12 +0100

    Rapport PDf

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -9,7 +9,7 @@ def create_app():
     
     app = Flask(__name__)
     
-    app.config["MONGO_URI"] = os.getenv("MONGO_URI", "mongodb://localhost:27017/healthguard")
+    app.config["MONGO_URI"] = os.getenv("MONGO_URI", "mongodb://healthguard-mongo:27017/healthguard")
     app.config["UPLOAD_FOLDER"] = "./uploads"
     app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
 
--- frontend/constants/api.ts
+++ frontend/constants/api.ts
@@ -6,7 +6,7 @@
 // Change this to your Flask backend URL
 // For iOS Simulator: use 127.0.0.1 (maps to host machine localhost)
 // For Physical iOS Device: use your machine IP (e.g., 192.168.21.211)
-const DEV_API_URL = "http://192.168.21.96:5000"; // Use local network IP
+const DEV_API_URL = "http://192.168.21.211:5000"; // Use local network IP
 const PROD_API_URL = "https://your-azure-api.azurecontainer.io/api";
 
 export const API_BASE_URL = DEV_API_URL; // or your actual API URL
